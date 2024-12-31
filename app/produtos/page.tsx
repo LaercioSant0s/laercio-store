1e12410948da98528ea430fde2f19b4cd9ff8cb4
Laércio Santos <laersantos13@hotmail.com>
2024-12-31 03:53:39 +0000

    working on Products

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import React from "react";
+import { useState, useEffect } from "react"; 
 import useSWR from "swr";
+import { Button } from "@nextui-org/react";
 import ProductListItem from "@/app/_components/cardComponent/product-list-item";
 import { ProdutosResposta } from "../_models/produtos";
 
@@ -12,16 +14,39 @@ export default function page() {
     "/api/produtos/",
     fetcher
   );
+  
+  const [search, setSearch] = useState("");
+  
+  const [filteredData, setFilteredData] = useState<ProdutosResposta[]>([])
+  
+  const [cart, setCart] = useState<ProdutosResposta[]>([])
 
+  useEffect(() => {
+    if (data) {
+      const newFilteredData = data.filter((product) => {
+        return product.title.toLowerCase().includes(search.toLowerCase());
+      })
+      setFilteredData(newFilteredData);
+    }
+  }, [search, data])
+  
+  
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
   if (data === undefined) return <div>error</div>;
-
+  
   return (
     <>
+      <div className="flex justify-center">
+        <input placeholder="Search by name"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)} 
+        className="rounded-md px-5"/>
+      </div>
+
       <div className="my-auto flex w-full max-w-7xl flex-col items-center gap-2 mx-auto">
         <div className="grid w-full grid-cols-1 gap-6 px-6 py-5 sm:grid-cols-2 lg:grid-cols-3">
-          {data.map((produto) => (
+          {filteredData.map((produto) => (
             <ProductListItem
               key={produto.id}
               id={produto.id}
